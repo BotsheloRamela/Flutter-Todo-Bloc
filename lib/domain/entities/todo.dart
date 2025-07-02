eeea432784e7847bd8eecf46e9800b2e2a563f76
BotsheloRamela <ramelabotshelo12@gmail.com>
2025-07-02 22:36:57 +0200

    chore: refactor todo domain entity
    - include createdAt in domain entity
    - remove description from domain entity
    - enhance _toDomain type safety by using TodoEntity as param type
    - throw error on ID parsing to avoid silent mask failure for invalid IDs

--- lib/domain/entities/todo.dart
+++ lib/domain/entities/todo.dart
@@ -5,30 +5,30 @@ class Todo extends Equatable {
   const Todo({
     required this.id,
     required this.title,
-    required this.description,
+    required this.createdAt,
     this.isCompleted = false,
   });
 
   final String id;
   final String title;
-  final String description;
   final bool isCompleted;
+  final String createdAt;
 
   Todo copyWith({
     String? id,
     String? title,
-    String? description,
     bool? isCompleted,
+    String? createdAt,
   }) => Todo(
     id: id ?? this.id,
     title: title ?? this.title,
-    description: description ?? this.description,
     isCompleted: isCompleted ?? this.isCompleted,
+    createdAt: createdAt ?? this.createdAt,
   );
 
   @override
-  List<Object?> get props => [id, title, description, isCompleted];
+  List<Object?> get props => [id, title, isCompleted, createdAt];
 
   @override
-  String toString() => 'Todo(id: $id, title: $title, description: $description, isCompleted: $isCompleted)';
+  String toString() => 'Todo(id: $id, title: $title, isCompleted: $isCompleted, createdAt: $createdAt)';
 }
