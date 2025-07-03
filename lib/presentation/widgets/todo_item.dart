50cd02a2117ea3a12dd92ea09543d662b2f8981a
BotsheloRamela <ramelabotshelo12@gmail.com>
2025-07-03 17:04:57 +0200

    feat(logging): integrate logger package
    - Added the logger package to manage logging throughout the application.
    - Implemented logging in TodoBloc to track actions and errors during todo operations.
    - Added Todo update handling to UI

--- lib/presentation/widgets/todo_item.dart
+++ lib/presentation/widgets/todo_item.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_todo_bloc/domain/entities/todo.dart';
 import 'package:flutter_todo_bloc/presentation/bloc/todo_bloc.dart';
 import 'package:flutter_todo_bloc/presentation/bloc/todo_event.dart';
+import 'package:flutter_todo_bloc/presentation/widgets/todo_dialog.dart';
 
 class TodoItem extends StatelessWidget {
   const TodoItem({required this.todo, super.key});
@@ -11,10 +12,14 @@ class TodoItem extends StatelessWidget {
   final Todo todo;
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: Convert time to a more readable format
-    return ListTile(
-      title: Text(todo.title),
+  Widget build(BuildContext context) => ListTile(
+      title: Text(
+        todo.title,
+        style: TextStyle(
+          decoration: todo.isCompleted ? TextDecoration.lineThrough : null,
+          color: todo.isCompleted ? Colors.grey : Colors.black,
+        ),
+      ),
       subtitle: Text('Created at: ${todo.createdAt}'),
       trailing: Checkbox(
         value: todo.isCompleted,
@@ -22,9 +27,17 @@ class TodoItem extends StatelessWidget {
           context.read<TodoBloc>().add(TodoCompletionToggled(todo.id!));
         },
       ),
+      onTap: () {
+        todoDialog(
+          context: context,
+          todo: todo,
+          onSave: (String title) {
+            context.read<TodoBloc>().add(TodoUpdated(todo.copyWith(title: title)));
+          },
+        );
+      },
       onLongPress: () {
         context.read<TodoBloc>().add(TodoDeleted(todo.id!));
       },
     );
-  }
 }
